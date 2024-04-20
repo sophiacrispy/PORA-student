35ca156a486ab49097c31b6e67d7a8dae53aa1a9
sophiacrispy <sophiacrispy1@gmail.com>
2024-04-20 14:54:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,169 @@
+
+//console.log("Hello world")
+
+//variables
+//let x= 5
+//let y= 10
+//let country = "Nigeria"
+
+//console.log(x)
+//console.log(y)
+//console.log(country)
+
+     
+//const names = "john 12345634"
+//const num = 23
+//const isMale = false
+
+//console.log(names)
+//console.log(num)
+//console.log(isMale)
+
+//let y = 10
+//let x = 5
+//let z = y + x
+
+//console.log(z)
+
+
+
+//const date = new Date()+1
+
+//console.log (date)
+
+//let firstName = "john"
+//let lastName = "peters"
+
+//let full_name = firstName +" " +lastName
+
+//console.log (full_name)
+
+//const x = 5
+//const y = 5
+
+//if(x===y){
+
+    //return console.log("its truth")
+
+//}
+
+//else{
+    //return console.log("its false")
+//}
+
+
+
+//function myFunction()  {
+
+    //const result = 2 * 4;
+
+   // console.log({result: "here is yor result" + "" + result})
+//}
+//myFunction()
+
+//function student(firstname, lastname, etc) {
+
+   //// const fulname = firstname + " " + lastname
+
+
+   // console.log ("full name of the stdents" + "" + fulname)
+//}
+
+//student(" peters", "obi",)
+
+
+
+
+//function multiplyNumbers(numb1, numb2) {
+
+    //const result = numb1 * numb2;
+
+    //return result
+    
+//}
+
+//console.log(multiplyNumbers(2, 3))
+//javascript Arrays
+//const car = {
+    //name: "toyota",
+    //color: "pink",
+    ////model: "classic230",
+
+//}
+
+//const cars = ["saab", "volvo", "BMW"];
+//const names = ["tobi", "ruth", "obi"];
+
+
+//const jss1a = ["cecilie", "lone", "roy"];
+//const jss1b = ["emil", "tobias", "linus"];
+
+//const jss1 = jss1a.concat(jss1b)
+
+//console.log(jss1)
+
+//const age1 = ["love", "john", "roy"];
+//const age2 = ["you", "yes"];
+//const age3 = ["lone"];
+
+//const children = age1.concat(age1, age2, age3)
+
+//console.log(children)
+
+//fill
+//const fruits = ["banana", "orange", "apple", "mango"];
+//console.log(fruits)
+
+//fruits.fill("kiwi", 2, 4);
+
+//console.log(fruits)
+
+//Arrays methods of properties
+// at()
+//concat()
+//constructor()
+//flat()
+//forEach()
+//fill
+
+//callback function
+
+//function myDisplayer(display) {
+    //console.log(display)
+//}
+
+////function myCalculator(num1, num2, myCallback) {
+    //let sum = num1 + num2;
+    //myCallback(sum);
+//}
+
+//myCalculator(5, 5, myDisplayer);
+
+//setTimeout(myDisplayer, 3000)
+
+//function myDisplayer(display) {
+    //console.log("i love you")
+//}
+
+//setInterval(myDisplayer,5000)
+
+//function myDisplayer(display) {
+    //console.log("i love you")
+//}
+
+//function myDisplayerNow(display) {
+    //console.log("Dis play now")
+//}
+
+//myDisplayerNow()
+
+
+
+//dev()
+
+const dev = (par1, par2) => {
+    console.log("dev")
+
+}
+
+dev(2,3)
